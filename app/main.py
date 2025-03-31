417e3db7e5b7c94b154e0912c37fc789b8358d65
Gurin_Valeriy <gurin_valerii@mail.ru>
2025-03-31 17:49:43 +0600

    02dev commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+import json
 
 from models import models
 
@@ -18,12 +19,12 @@ def read_custom_message():
 
 
 @app.get("/users")
-def read_custom_message():
+def read_users_dict():
     return models.User(name="John Doe", id=1)
 
 
 @app.post("/user")
-def read_custom_message(user: models.User):
+def add_user_to_dict(user: models.User):
     usr = user.__dict__
     usr.update({"is_adult": user.age >= 18})
     return usr
@@ -31,7 +32,27 @@ def read_custom_message(user: models.User):
     #         "age": user.age,
     #         "is_adult": user.age>=18}
 
+def json_dumping(path: str, feed_db: dict) -> None:
+    with open(path, 'w', encoding='U8') as ofl:
+        json.dump(feed_db, ofl, indent=4, ensure_ascii=False, separators=(',', ': '))
 
+def json_loading(path: str) -> dict:
+    try:
+        with open(path, 'r', encoding='U8') as ifl:
+            return json.load(ifl)
+    except Exception:
+        return {"feedbacks":[]}
+
+@app.post("/feedback")
+def post_feedback(feedback: models.Feedback):
+    global feedb_db_path
+    fbacks_db["feedbacks"].append(feedback.__dict__)
+    json_dumping(feedb_db_path, fbacks_db)
+    return {"message": f"Feedback received. Thank you, {feedback.name}!"}
+
+
+feedb_db_path = 'feedbacks_db.json'
+fbacks_db = json_loading(feedb_db_path)
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, app_dir=".app")
     # uvicorn.run(app, port=8000, reload=True)
--- models/models.py
+++ models/models.py
@@ -8,3 +8,7 @@ class User(BaseModel):
     # id: int
     name: str = ""
     age: int
+
+class Feedback(BaseModel):
+    name: str
+    message: str
